542926e0933d136ef36975344189c3e8a5f31b85
Cédric Menzi <cedric.menzi@buhlergroup.com>
2021-04-16 07:22:16 +0200

    code added

---
+++ src/StrykerRepro/SupportedTypes.cs
@@ -0,0 +1,47 @@
+﻿namespace StrykerRepro
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+
+    using Newtonsoft.Json.Linq;
+
+    internal sealed partial class SupportedTypes : ReadOnlyDictionary<Type, string>
+    {
+        public static readonly SupportedTypes Map = new SupportedTypes();
+
+        public static bool IsSupported(Type type)
+        {
+            return Map.ContainsKey(type);
+        }
+
+        public static bool IsSupported(object value)
+        {
+            var valueToCheck = value;
+            if (valueToCheck == null)
+            {
+                return true;
+            }
+
+            var valueType = valueToCheck.GetType();
+            if (valueType.IsArray)
+            {
+                return IsSupported(valueType.GetElementType());
+            }
+
+            if (valueToCheck is JArray jarray)
+            {
+                valueToCheck = jarray.ToObject<IEnumerable<object>>();
+            }
+
+            if (valueToCheck is IEnumerable enumerable)
+            {
+                return enumerable.OfType<object>().All(x => IsSupported(x.GetType()));
+            }
+
+            return IsSupported(valueType);
+        }
+    }
+}
